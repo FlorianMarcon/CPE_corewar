63e69b42101796e951bb774486ec27db05dec61d
marcon <florian.marcon@epitech.eu>
2018-02-23 10:30:10 +0100

    create function for verify all instruction

---
+++ assembleur/src/verification/verification_instruction.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2017
+** verification_instruction
+** File description:
+** verification_instruction
+*/
+
+#include "header_asm.h"
+#include "op.h"
+int	len_tab(char **tab);
+int	verification_argument(int op_code, char *argu);
+int	search_function(char *str);
+
+int	verification_instruction(char **tab)
+{
+	int op_c;
+	int count;
+
+	if (len_tab(tab) != 2 || (op_c = search_function(tab[0])) == 0)
+		return (0);
+	else if ((count = verification_argument(op_c, tab[1])) == 0)
+		return (0);
+	else
+		return (count);
+}
