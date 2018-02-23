4b5203fc274627d5d1af09b53603a87e1cd6d335
marcon <florian.marcon@epitech.eu>
2018-02-23 10:41:06 +0100

    unit test for verify_instruction

--- assembleur/src/verification/verification_instruction.c
+++ assembleur/src/verification/verification_instruction.c
@@ -5,6 +5,7 @@
 ** verification_instruction
 */
 
+#include <stdlib.h>
 #include "header_asm.h"
 #include "op.h"
 int	len_tab(char **tab);
@@ -16,6 +17,8 @@ int	verification_instruction(char **tab)
 	int op_c;
 	int count;
 
+	if (tab == NULL)
+		return (0);
 	if (len_tab(tab) != 2 || (op_c = search_function(tab[0])) == 0)
 		return (0);
 	else if ((count = verification_argument(op_c, tab[1])) == 0)
--- assembleur/tests/verification/tests_utils.c
+++ assembleur/tests/verification/tests_utils.c
@@ -7,6 +7,7 @@
 
 #include <criterion/criterion.h>
 int	verification_argument(int op_code, char *argu);
+int	verification_instruction(char **tab);
 
 Test(verification_argument, test1)
 {
@@ -15,4 +16,18 @@ Test(verification_argument, test1)
 	cr_assert_eq(verification_argument(3, "r2,r1"), 1);
 	cr_assert_eq(verification_argument(3, "r2,r1,r5"), 0);
 	cr_assert_eq(verification_argument(3, "r2,%2"), 0);
+}
+
+Test(verification_instruction, test1)
+{
+	char *tab1[3] = {"ld", "%1,r1", NULL};
+	char *tab2[3] = {"ld", "%1,%1", NULL};
+	char *tab3[3] = {"l", "%1,r1", NULL};
+	char *tab4[3] = {"ld", NULL};
+
+	cr_assert_eq(verification_instruction(tab1), 1);
+	cr_assert_eq(verification_instruction(tab2), 0);
+	cr_assert_eq(verification_instruction(tab3), 0);
+	cr_assert_eq(verification_instruction(tab4), 0);
+	cr_assert_eq(verification_instruction(NULL), 0);
 }
